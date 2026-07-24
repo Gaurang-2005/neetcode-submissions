7f37a4e474ff2118e22b1d4ebd9cbfca10b663e2
Gaurang-2005 <149991661+Gaurang-2005@users.noreply.github.com>
2026-07-24 23:36:08 +0530

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        if (s.length()%2 == 1) return false;
+        stack<char> store;
+
+        for (auto i : s) {
+            if (i == '(' || i == '{' || i == '[') {
+                store.push(i);
+                continue;
+            }
+            if (store.empty()) return false;
+            char t = store.top();
+            if (t == '(' && i == ')' || t == '{' && i == '}' || t == '[' && i == ']') store.pop();
+            else return false;
+        }
+        return store.empty();
+    }
+};
