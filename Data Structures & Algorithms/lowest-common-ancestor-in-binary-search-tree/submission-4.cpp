3a99b45fae40e8c792b71853dc46e59cf93bd8f0
Gaurang Gupta <149991661+Gaurang-2005@users.noreply.github.com>
2026-08-21 22:57:31 +0530

    Add: lowest-common-ancestor-in-binary-search-tree - submission-4

---
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-4.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        vector<TreeNode*> pathP;
+        vector<TreeNode*> pathQ;
+
+        auto temp = root;
+
+        while (temp != p) {
+            pathP.push_back(temp);
+            if (p -> val < temp -> val) {
+                temp = temp -> left;
+                continue;
+            }
+            else if (p -> val > temp -> val) {
+                temp = temp -> right;
+                continue;
+            }
+        }
+        pathP.push_back(temp);
+        temp = root;
+        while (temp != q) {
+            pathQ.push_back(temp);
+            if (q -> val < temp -> val) {
+                temp = temp -> left;
+                continue;
+            }
+            else if (q -> val > temp -> val) {
+                temp = temp -> right;
+                continue;
+            }
+        }
+        pathQ.push_back(temp);
+        vector<TreeNode*> min, max;
+        if (pathP.size() < pathQ.size()) {
+            min = pathP;
+            max = pathQ;
+        }
+        else {
+            max = pathP;
+            min = pathQ;
+        }
+        for (int i = max.size() - 1; i >= 0; i--) {
+            for (int j = min.size() - 1; j >= 0; j--) {
+                if (min[j] == max[i]) return min[j];
+            }
+        } 
+        return nullptr;        
+    }
+};
